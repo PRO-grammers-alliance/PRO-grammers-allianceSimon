c688a595a4a376cbfa2553d99dd39fce8c304e01
David Real <81327257+DavidReal04@users.noreply.github.com>
2021-04-16 17:41:21 -0500

    Comentarios del funcionamiento de los Métodos en Operaciones y Secuencia

--- ProyectoSimon/src/co/edu/unbosque/model/Operaciones.java
+++ ProyectoSimon/src/co/edu/unbosque/model/Operaciones.java
@@ -4,13 +4,14 @@ import co.edu.unbosque.model.persistence.Propiedades;
 
 public class Operaciones{
 	
+	//Método que retorna un String de los números generados que se guardaron en el arreglo en la clase Secuencia
 	public String crearSecuencia() {
 		Secuencia sec = new Secuencia();
 		sec.generarNumeros();
 		String secuencia = sec.devolverArreglo();
 		return secuencia;
 	}
-	
+	//Metodo que recibe por parámetro un String para guardar en el archivo de properties
 	public void gestionarPropiedades(String numeros) {
 		Propiedades prop = new Propiedades();
 		prop.escribirPropiedades(numeros);
--- ProyectoSimon/src/co/edu/unbosque/model/Secuencia.java
+++ ProyectoSimon/src/co/edu/unbosque/model/Secuencia.java
@@ -8,13 +8,14 @@ public class Secuencia {
 		secuencia = new String[4];
 	}
 	
+	//Método que genera y guarda un número entero random entre 1 y 4 por cada posición del Arreglo 
 	protected void generarNumeros() {
 		for (int i = 0; i < secuencia.length; i++) {
 			long numeros = Math.round(Math.random()*3+1);
 			secuencia[i] = String.valueOf(numeros);
 		}
 	}
-
+	//Método que junta en un String los números en cada posición del arreglo
 	protected String devolverArreglo() {
 		String texto="";
 		for (int i = 0; i < getSecuencia().length; i++) {
